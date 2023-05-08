f662aceb8630c6e6def44e7be5c0a474995396bc
BrenoBar51 <brenobarcelos9@gmail.com>
2023-05-08 09:00:03 -0300

    commit inicial

---
+++ condicional.js
@@ -0,0 +1,49 @@
+/* numeros maiores e menores
+const num1 = prompt('coloque aqui um numero')
+const num2 = prompt('coloque aqui um numero')
+
+if(num1 > num2){
+    alert('menor')
+}
+else if(num1 < num2){
+    alert('maior')
+}
+else{
+    alert('igual')
+}*/
+/* idade para dirigir
+const idade = prompt('coloque sua idade aqui')
+
+if(idade >= 18){
+    alert('Tem idade pra dirigir')
+}
+else{
+    alert('Não tem idade para dirigir')
+}*/
+/* par e impar
+const multi = parN % 2
+if(multi === 0){
+    alert('numero par')
+}
+else{
+    alert('numero impar')
+}*/
+/*const nota = prompt('coloque sua nota')
+
+if(nota <= 5){
+    alert('não passou')
+}
+else if(nota >= 6){
+    alert('passou')
+}*/
+/*const num = prompt('numero aqui')
+
+if(num > 0){
+    alert('Positivo')
+}
+else if(num < 0){
+    alert('Negativo')
+}
+else{
+    alert('Zero')
+}*/
